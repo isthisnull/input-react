b8ed19ba0996090e7587162d881643685873bfc0
isthisnull <letscodewithzanjiri@hotmail.com>
2023-10-21 11:48:20 +0300

    1

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,23 @@
 import colorNames from "colornames";
 import React, { useState } from "react";
+import "./App.css";
 function Button() {
   const [colr, setColr] = useState("");
   return (
     <>
+      <section className="mamad" style={{ backgroundColor: colr }}>
+        ez
+      </section>
+
+      <input
+        type="text"
+        required
+        autoFocus
+        value={colr}
+        onChange={(e) => setColr(e.target.value)}
+      />
       <p style={{ backgroundColor: colr }}>kir</p>
-      <button onClick={() => setColr("Blue")}>red misham</button>
+      <button onClick={() => setColr("white")}>red misham</button>
     </>
   );
 }
